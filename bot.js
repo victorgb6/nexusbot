c84de2ca35211c963883b6488e28c7f40896f4c4
Victor Gil Borrego <victorgb6@gmail.com>
2015-12-27 17:53:19 +0100

    add giphy

--- bot.js
+++ bot.js
@@ -1,5 +1,6 @@
 // An example for OpenShift platform.
 var TelegramBot = require('node-telegram-bot-api');
+var request = require('request');
 
 var token = '174209263:AAFz6nGIWjyMKjTWGe4ewy59qGr189DmtKw';
 // See https://developers.openshift.com/en/node-js-environment-variables.html
@@ -11,10 +12,25 @@ var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
 // OpenShift enroutes :443 request to OPENSHIFT_NODEJS_PORT
 bot.setWebHook(domain+':443/bot'+token);
 console.log('webhook set!');
-bot.on('message', function (msg) {
-  console.log('Msg received->', msg);
+// bot.on('message', function (msg) {
+//   console.log('Msg received->', msg);
+//   var chatId = msg.chat.id;
+//   bot.sendMessage(chatId, "I'm alive!");
+// });
+
+//Giphy
+bot.onText(/\/giphy/, function(msg) {
   var chatId = msg.chat.id;
-  bot.sendMessage(chatId, "I'm alive!");
+  var search = msg.text.split("/giphy ")[1];
+  if (search) {
+    request('http://api.giphy.com/v1/gifs/translate?s='+search+'&api_key=dc6zaTOxFJmzC', function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        body = JSON.parse(body);
+        console.log(body);
+        bot.sendMessage(chatId, body.data.images.original.url);
+      }
+    });
+  }
 });
 
 // Matches /love
