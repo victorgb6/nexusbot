fd7f59f82aba649b8355095d65bdfe50af289d42
Victor Gil Borrego <victorgb6@gmail.com>
2015-12-29 01:46:30 +0100

    firebase adde

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
-// An example for OpenShift platform.
+'use strict';
 var TelegramBot = require('node-telegram-bot-api');
 var request = require('request');
+var Firebase = require("firebase");
 
 var token = '174209263:AAFz6nGIWjyMKjTWGe4ewy59qGr189DmtKw';
 // See https://developers.openshift.com/en/node-js-environment-variables.html
@@ -12,14 +13,14 @@ var bot = new TelegramBot(token, {webHook: {port: port, host: host}});
 // OpenShift enroutes :443 request to OPENSHIFT_NODEJS_PORT
 bot.setWebHook(domain+':443/bot'+token);
 console.log('webhook set!');
-// bot.on('message', function (msg) {
-//   console.log('Msg received->', msg);
-//   var chatId = msg.chat.id;
-//   bot.sendMessage(chatId, "I'm alive!");
-// });
 
-//Giphy
+var saveUser = function() {
+
+};
+
+//Giphy command
 bot.onText(/\/giphy/, function(msg) {
+  console.log('Msg->',msg);
   var chatId = msg.chat.id;
   var search = msg.text.split("/giphy ")[1];
   if (search) {
@@ -31,18 +32,4 @@ bot.onText(/\/giphy/, function(msg) {
       }
     });
   }
-});
-
-// Matches /love
-bot.onText(/\/love/, function (msg) {
-  var chatId = msg.chat.id;
-  var opts = {
-      reply_to_message_id: msg.message_id,
-      reply_markup: JSON.stringify({
-        keyboard: [
-          ['Yes, you are the bot of my life ❤'],
-          ['No, sorry there is another one...']]
-      })
-    };
-    bot.sendMessage(chatId, 'Do you love me?', opts);
 });
