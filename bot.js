3f7902d687abda799961cc175df4c8515041639a
Victor Gil Borrego <victorgb6@gmail.com>
2016-01-06 16:33:36 +0100

    tostring

--- bot.js
+++ bot.js
@@ -110,7 +110,7 @@ bot.onText(/\/pinpon accept/, function(msg) {
   match = {};
   challengesRef.once("value", function(snapshot) {
     snapshot.forEach(function(childSnapshot) {
-      if ( userFromId.indexOf( childSnapshot.key() ) !== -1) {
+      if ( userFromId.toString().indexOf( childSnapshot.key() ) !== -1) {
         found = true;
         challenge = childSnapshot;
       }
