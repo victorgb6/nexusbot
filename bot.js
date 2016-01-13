d2ff482038ce633fabcd7cbe89f709eea0c2b680
Victor Gil Borrego <victorgb6@gmail.com>
2016-01-13 21:01:24 +0100

    report

--- bot.js
+++ bot.js
@@ -198,9 +198,11 @@ bot.onText(/\/report/, function(msg) {
   console.log('MSG report->',msg);
   var chatId = msg.chat.id,
   userFromId = msg.from.id,
-  userTo = msg.text.split('/report ')[1].split('@')[1],
+  arguments = msg.text.split('/report ')[1]
+  userTo = arguments.split('@')[1],
   score = msg.text.split('/report ')[2],
   sets = score.split(","),
   matchesRef = fireRef.child("matches");
+
   console.log('userTo->',userTo, 'score->',score);
 });
