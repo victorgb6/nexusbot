f814410ab3a966ce4b83114f0a9c7ff3c179a487
Victor Gil Borrego <victor.gil@lbigroup.es>
2016-01-14 12:36:26 +0100

    accept

--- bot.js
+++ bot.js
@@ -38,6 +38,7 @@ var saveChallenge = function(userFrom, userFromId, userTo, userToId) {
 
 var saveMatch = function(chatId, challenge) {
   var matchesRef = fireRef.child("matches"),
+  challengesRef = fireRef.child("challenges")
   match = {};
   console.log('Challenge->',challenge);
   match.userFromId = challenge.userFromId;
