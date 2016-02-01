3a980dc6da5339cc2c50f8664ce6af8881e39a1a
Victor Gil Borrego <victorgb6@gmail.com>
2016-02-01 21:00:47 +0100

    challenge

--- app.js
+++ app.js
@@ -105,15 +105,15 @@ bot.onText(/\/accept/, function(msg) {
     match.winner  = false;
     db.saveMatch(match).then(function(){
       db.removeChallenge(challengedId);
-      bot.sendMessage(chatId, "Challenge accepted. Let's Play.";
-      bot.sendMessage(challenge.userFromId, "Your challenge to @"+msg.chat.username+" has been accepted. Get ready!";
+      bot.sendMessage(chatId, "Challenge accepted. Let's Play.");
+      bot.sendMessage(challenge.userFromId, "Your challenge to @"+msg.chat.username+" has been accepted. Get ready!");
     }, function() {
       console.log('Error saving the match.');
-      bot.sendMessage(chatId, "There was an error creating your match.";
+      bot.sendMessage(chatId, "There was an error creating your match.");
     });
   }, function(){
     console.log('Error accepting challenge');
-    bot.sendMessage(chatId, "You don't have any challenge to accept.";
+    bot.sendMessage(chatId, "You don't have any challenge to accept.");
   });
 });
 
