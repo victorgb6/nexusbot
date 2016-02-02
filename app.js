3774f034213f46da6d043483a888dfc8011e6dc3
Victor Gil Borrego <victorgb6@gmail.com>
2016-02-02 18:32:55 +0100

    loss

--- app.js
+++ app.js
@@ -97,32 +97,28 @@ bot.onText(/\/challenge/, function(msg, match) {
 bot.onText(/\/accept/, function(msg) {
   console.log('MSG accept->',msg);
   var chatId = msg.chat.id;
-  db.getPendingMatch(chatId).then(function(hasPendingMatch){
-    if (!hasPendingMatch) {
-      db.getChallenge(chatId).then(function(challenge){
-        console.log('Accept challenge:', challenge);
-        var match = {};
-        match.host    = challenge.userFromId;
-        match.visitor = chatId;
-        match.result  = false;
-        match.winner  = false;
-        db.saveMatch(match).then(function(matchKey){
-          db.removeChallenge(chatId, matchKey);
-          bot.sendMessage(chatId, "Challenge accepted. Let's Play.");
-          bot.sendMessage(challenge.userFromId, "Your challenge to @"+msg.chat.username+" has been accepted. Get ready!");
-        }, function() {
-          console.log('Error saving the match.');
-          bot.sendMessage(chatId, "There was an error creating your match.");
-        });
-      }, function(){
-        console.log('Error accepting challenge');
-        bot.sendMessage(chatId, "You don't have any challenge to accept.");
-      });
-    } else {
-      bot.sendMessage(chatId, "You cannot accept a challenge because you have an unfinished match. Report the result or resign the match and try again.");
-    }
-  }, function() {
+  db.getPendingMatch(chatId).then(function(){
     console.log('Error checking if hasPendingMatch');
+  }, function() {
+    db.getChallenge(chatId).then(function(challenge){
+      console.log('Accept challenge:', challenge);
+      var match = {};
+      match.host    = challenge.userFromId;
+      match.visitor = chatId;
+      match.result  = false;
+      match.winner  = false;
+      db.saveMatch(match).then(function(matchKey){
+        db.removeChallenge(chatId, matchKey);
+        bot.sendMessage(chatId, "Challenge accepted. Let's Play.");
+        bot.sendMessage(challenge.userFromId, "Your challenge to @"+msg.chat.username+" has been accepted. Get ready!");
+      }, function() {
+        console.log('Error saving the match.');
+        bot.sendMessage(chatId, "There was an error creating your match.");
+      });
+    }, function(){
+      console.log('Error accepting challenge');
+      bot.sendMessage(chatId, "You don't have any challenge to accept.");
+    });
   });
 
 });
