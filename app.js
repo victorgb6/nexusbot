26096047b80a8b4055680b3af65f6a62dc1087a6
Alberto Fernandez <(null)>
2016-02-05 15:57:39 +0100

    Externalize challenge function

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var TelegramBot = require('node-telegram-bot-api');
 var Firebase    = require('firebase');
 var db          = require('./lib/db');
 var register    = require('./bot/register');
+var challenge   = require('./bot/challenge');
 
 var token = process.env.telegram_token;
 // See https://developers.openshift.com/en/node-js-environment-variables.html
@@ -24,37 +25,10 @@ bot.setWebHook(domain + ':443/bot' + token);
 console.log('webhook set!');
 
 //Register user to firebase
-bot.onText(/\/register/, register(msg, match));
+bot.onText(/\/register/, register);
 
 //challenge another user
-bot.onText(/\/challenge/, function(msg, match) {
-  console.log('MSG->',msg,'MATCH->',match);
-  var chatId = msg.chat.id,
-  userFromId = msg.from.id,
-  userFrom = msg.from.username,
-  userToId = null,
-  userTo = msg.text.split('/challenge @')[1];
-
-  db.findUserById(userFromId).then(function(){
-    db.findUserByName(userTo.toLowerCase()).then(function(user){
-      var challenge = {userFrom: userFrom,
-                       userFromId: userFromId
-                     };
-      db.saveChallenge(user.key(), challenge).then(function(){
-        bot.sendMessage(user.key(), '@'+userTo+' you have been challenge by @'+userFrom+' type /accept or /decline to get started.');
-        bot.sendMessage(userFromId, 'Your challenge has been sent to @'+userTo);
-      }, function() {
-        bot.sendMessage(userFromId, 'Challenge cannot be saved');
-      });
-    }, function(){
-      console.log('Challenged user doesn\'t exists.');
-      bot.sendMessage(chatId, 'Challenged user @'+userTo+' is not registered yet.');
-    });
-  }, function() {
-    console.log('User doesn\'t exists.');
-    bot.sendMessage(chatId, '@' + userFromId + ' is not registered yet.');
-  });
-});
+bot.onText(/\/challenge/, challenge);
 
 //accept a challenge
 bot.onText(/\/accept/, function(msg) {
---
+++ bot/challenge.js
@@ -0,0 +1,30 @@
+var challenge = function(msg, match) {
+  console.log('MSG->',msg,'MATCH->',match);
+  var chatId = msg.chat.id,
+  userFromId = msg.from.id,
+  userFrom = msg.from.username,
+  userToId = null,
+  userTo = msg.text.split('/challenge @')[1];
+
+  db.findUserById(userFromId).then(function(){
+    db.findUserByName(userTo.toLowerCase()).then(function(user){
+      var challenge = {userFrom: userFrom,
+                       userFromId: userFromId
+                     };
+      db.saveChallenge(user.key(), challenge).then(function(){
+        bot.sendMessage(user.key(), '@'+userTo+' you have been challenge by @'+userFrom+' type /accept or /decline to get started.');
+        bot.sendMessage(userFromId, 'Your challenge has been sent to @'+userTo);
+      }, function() {
+        bot.sendMessage(userFromId, 'Challenge cannot be saved');
+      });
+    }, function(){
+      console.log('Challenged user doesn\'t exists.');
+      bot.sendMessage(chatId, 'Challenged user @'+userTo+' is not registered yet.');
+    });
+  }, function() {
+    console.log('User doesn\'t exists.');
+    bot.sendMessage(chatId, '@' + userFromId + ' is not registered yet.');
+  });
+};
+
+module.exports = challenge;
