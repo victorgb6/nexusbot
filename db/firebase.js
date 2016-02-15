be4e083b2dca2de82add249a7349d188306a107b
Victor Gil Borrego <victorgb6@gmail.com>
2016-02-15 21:04:10 +0100

    add firebase auth

--- db/firebase.js
+++ db/firebase.js
@@ -1,12 +1,12 @@
 var Firebase = require('firebase');
 var fireRef = new Firebase('https://nexus-bot.firebaseio.com/');
 
-// fireRef.authWithCustomToken(process.env.FIREBASE_SECRET, function(error, authData) {
-//   if (error) {
-//     console.log("Authentication Failed!", error);
-//   } else {
-//     console.log("Authenticated successfully with payload:", authData);
-//   }
-// });
+fireRef.authWithCustomToken(process.env.FIREBASE_SECRET, function(error, authData) {
+  if (error) {
+    console.log("Authentication Failed!", error);
+  } else {
+    console.log("Authenticated successfully with payload:", authData);
+  }
+});
 
 module.exports = fireRef;
