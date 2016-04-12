3d02c0c3294f869a07ef3019344d44dea9b3d986
Victor Gil Borrego <victor.gil@lbigroup.es>
2016-04-12 12:17:08 +0200

    regexp

--- app.js
+++ app.js
@@ -8,22 +8,22 @@ var lost          = require('./bot/lost');
 var registerTeam  = require('./bot/registerTeam');
 
 //Register user to firebase
-bot.onText(/\bregister\b/i, register);
+bot.onText(/\/\bregisterteam\b/i, register);
 
 //challenge another user
-bot.onText(/\bchallenge\b/i, challenge);
+bot.onText(/\/\bregisterteam\b/i, challenge);
 
 //accept a challenge
-bot.onText(/\baccept\b/i, accept);
+bot.onText(/\/\bregisterteam\b/i, accept);
 
 //decline a challenge
-bot.onText(/\bdecline\b/i, decline);
+bot.onText(/\/\bregisterteam\b/i, decline);
 
 //resign a match
-bot.onText(/\bresign\b/i, resign);
+bot.onText(/\/\bregisterteam\b/i, resign);
 
 //Report a match
-bot.onText(/\blost\b/i, lost);
+bot.onText(/\/\bregisterteam\b/i, lost);
 
 //Register a team to firebase
-bot.onText(/registerTeam/i, registerTeam);
+bot.onText(/\/\bregisterteam\b/i, registerTeam);
