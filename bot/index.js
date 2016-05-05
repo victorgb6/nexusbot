875346c2fc7292c724cb29c1785b645e95f80ee7
Victor Gil Borrego <victor.gil@lbigroup.es>
2016-05-05 13:15:17 +0200

    Edit connection details to digitalocean

--- bot/index.js
+++ bot/index.js
@@ -1,20 +1,16 @@
 var TelegramBot = require('node-telegram-bot-api');
 
 var token = process.env.TELEGRAM_TOKEN;
-// See https://developers.openshift.com/en/node-js-environment-variables.html
-var port = process.env.OPENSHIFT_NODEJS_PORT;
-var host = process.env.OPENSHIFT_NODEJS_IP;
-var domain = process.env.OPENSHIFT_APP_DNS;
 
 var bot = new TelegramBot(token, {
   webHook: {
-    port: port,
-    host: host
+    port: 3000,
+    host: '127.0.0.1'
   }
 });
 
 // OpenShift enroutes :443 request to OPENSHIFT_NODEJS_PORT
-bot.setWebHook(domain + ':443/bot' + token);
+bot.setWebHook('https://victorgil.me' + '/bot' + token);
 console.log('webhook set!');
 
 module.exports = bot;
