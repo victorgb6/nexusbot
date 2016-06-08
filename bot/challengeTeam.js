d9d714c140f0bb56efacedbef69c43f1771bc9a0
Victor Gil Borrego <victorgb6@gmail.com>
2016-06-08 22:22:33 +0200

    work challengeTeam

--- bot/challengeTeam.js
+++ bot/challengeTeam.js
@@ -1,14 +1,17 @@
-
+var teams = require('../db/teams');
 var bot = require('./index');
 
 
 var challengeTeam = function(msg, match){
   console.log('MSG challengeTeam->',msg);
-  // teams.findByName(teamId).then(function(team){
-  //   var challenge = {teamFromName: '',
-  //                    teamFromId: ''};
-  //   console.log('Found Team->', team);
-  // });
+  var chatId       = msg.chat.id;
+  var userFromId   = msg.chat.id;
+  var team         = msg.text.split('/challengeTeam ')[1];
+  teams.findByName(teamId).then(function(team){
+    var challenge = {teamFromName: '',
+                     teamFromId: ''};
+    console.log('Found Team->', team);
+  });
   //bot.sendMessage(chatId, 'Challenged user @' + userTo + ' is not registered yet.');
 };
 
