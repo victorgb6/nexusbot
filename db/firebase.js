683f221f8e0b937ae35828e87e598f9a3db30398
Victor Gil Borrego <victor.gil@lbigroup.es>
2016-09-02 13:37:40 +0200

    Changed firebase log

--- db/firebase.js
+++ db/firebase.js
@@ -3,9 +3,9 @@ var fireRef = new Firebase('https://nexus-bot.firebaseio.com/');
 
 fireRef.authWithCustomToken(process.env.FIREBASE_SECRET, function(error, authData) {
   if (error) {
-    console.log("Authentication Failed!", error);
+    console.log("Firebase: Authentication Failed!", error);
   } else {
-    console.log("Authenticated successfully with payload:", authData);
+    console.log("Firebase: Authenticated successfully with payload:", authData);
   }
 });
 
