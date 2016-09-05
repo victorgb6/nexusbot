05c0498e823840556d21af3c1caf4af700e7d5c4
Victor Gil Borrego <victorgb6@gmail.com>
2016-09-05 21:49:48 +0200

    Revert "added rank"
    This reverts commit f269d28d12eb2ee2a57cd924c954d21c3bf21508.

---
+++ bot/rank.js
@@ -0,0 +1,24 @@
+var bot   = require('./index');
+var utils = require('../utils/utils');
+var users = require('../db/users');
+
+var rank = function(msg, match) {
+  console.log('Rank MSG->',msg);
+  var chatId   = msg.chat.id;
+  var usersArr = msg.text.split('@').slice(1);
+
+  if (usersArr.length == 0) {
+    usersArr.push(msg.from.username);
+  }
+
+  usersArr.map(function(name){
+    users.findByName(name).then(function(u){
+      var res = u.username + ': wins ' + u.wins + ' , loses ' + u.loses;
+      bot.sendMessage(chatId, res);
+    }, function(){
+      bot.sendMessage(chatId, 'Something went wrong getting your rank.');
+    })
+  })
+};
+
+module.exports = rank;
