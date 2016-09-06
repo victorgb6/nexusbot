6f29e38e1eea7741736b4d8c77bac7e771b8b640
Victor Gil Borrego <victor.gil@lbigroup.es>
2016-09-06 13:21:07 +0200

    rank

--- bot/rank.js
+++ bot/rank.js
@@ -10,16 +10,28 @@ var rank = function(msg, match) {
   if (usersArr.length == 0) {
     usersArr.push(msg.from.username);
   }
-
+  var queue = Q.defer();
+  var ranks = [];
   usersArr.map(function(name){
-    console.log('Rank getting: ', name);
-    return users.findByName(name).then(function(u){
-        console.log('rank u:', u.val());
-        u = u.val();
-        var res = u.username + ': wins ' + u.wins + ' , loses ' + u.loses;
-        bot.sendMessage(chatId, res);
+    queue = queue.then(function(u){
+      u = u.val();
+      ranks.push()
+      return users.findByName(name)
+    });
+    /*console.log('Rank getting: ', name);
+    users.findByName(name).then(function(u){
+      console.log('rank u:', u.val());
+      u = u.val();
+      var res = u.username + ': wins ' + u.wins + ' , loses ' + u.loses;
+      bot.sendMessage(chatId, res);
     }, function(){
       bot.sendMessage(chatId, 'Something went wrong getting your rank.');
+    });*/
+  });
+  queue.then(function(){
+    ranks.map(function(u){
+      var res = u.username + ': wins ' + u.wins + ' , loses ' + u.loses;
+      bot.sendMessage(chatId, res);
     })
   })
 };
