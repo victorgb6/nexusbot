77759908691be15972cc25ba7a7552fdf7b31c6c
Victor Gil Borrego <victor.gil@lbigroup.es>
2016-09-06 14:40:21 +0200

    rank

--- bot/rank.js
+++ bot/rank.js
@@ -5,9 +5,10 @@ var users = require('../db/users');
 var rank = function(msg, match) {
   console.log('Rank MSG->',msg);
   var chatId   = msg.chat.id;
+  var user = msg.text.split('@').slice(1)[0] || msg.from.username;
 
-  console.log('Rank getting: ', name);
-  users.findByName(msg.from.username).then(function(u){
+  console.log('Rank getting: ', user);
+  users.findByName(user).then(function(u){
     console.log('rank u:', u.val());
     u = u.val();
     var res = u.username + ': wins ' + u.wins + ' , loses ' + u.loses;
