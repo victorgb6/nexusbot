cf6512f9084499f712e453bc346327e8f1615f26
Victor Gil Borrego <victor.gil@lbigroup.es>
2016-09-06 14:34:47 +0200

    rank

--- bot/rank.js
+++ bot/rank.js
@@ -1,7 +1,6 @@
 var bot   = require('./index');
 var utils = require('../utils/utils');
 var users = require('../db/users');
-var Q     = require('q');
 
 var rank = function(msg, match) {
   console.log('Rank MSG->',msg);
@@ -11,28 +10,17 @@ var rank = function(msg, match) {
   if (usersArr.length == 0) {
     usersArr.push(msg.from.username);
   }
-  var queue = [];
-  usersArr.map(function(name){
-    console.log('Rank getting: ', name);
-    queue.push(users.findByName(name));
-    /*users.findByName(name).then(function(u){
-      console.log('rank u:', u.val());
-      u = u.val();
-      var res = u.username + ': wins ' + u.wins + ' , loses ' + u.loses;
-      bot.sendMessage(chatId, res);
-    }, function(){
-      bot.sendMessage(chatId, 'Something went wrong getting your rank.');
-    });*/
-    console.log('Queue:', queue);
+
+  console.log('Rank getting: ', name);
+  users.findByName(name).then(function(u){
+    console.log('rank u:', u.val());
+    u = u.val();
+    var res = u.username + ': wins ' + u.wins + ' , loses ' + u.loses;
+    bot.sendMessage(chatId, res);
+  }, function(){
+    bot.sendMessage(chatId, 'Something went wrong getting your rank.');
   });
-  Q.all(queue).done(function (values) {
-    values.map(function(u){
-      console.log('rank u:', u.val());
-      u = u.val();
-      var res = u.username + ': wins ' + u.wins + ' , loses ' + u.loses;
-      bot.sendMessage(chatId, res);
-    });
-});
+  
 };
 
 module.exports = rank;
